201461097d9188c6a4ef4911e7d85656ce2f3b97
Kushal Kesharwani <kanhakesharwani23@gmail.com>
2025-09-25 23:48:02 +0530

    going live V0.4 || ACCOM LIVE || EVENTS MODEL UPDATE

--- src/app.js
+++ src/app.js
@@ -81,19 +81,19 @@ app.use((req, res, next) => {
 
 // Main CORS
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 
-//app.use((req, res, next) => {
-//  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-//  res.header("Access-Control-Allow-Credentials", "true");
-//  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With");
-//  if (req.method === "OPTIONS") {
-//    return res.sendStatus(204);
-//  }
-//  next();
-//});
+app.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+ res.header("Access-Control-Allow-Credentials", "true");
+ res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+ res.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With");
+ if (req.method === "OPTIONS") {
+   return res.sendStatus(204);
+ }
+ next();
+});
 
 
 
